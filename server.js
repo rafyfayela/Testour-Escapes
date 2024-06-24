a4729fc010a9f94055b8d3048595117b8c1bbd75
Rafy Fayela <fayelarafy@gmail.com>
2024-06-24 17:49:59 +0200

    unhandled rejection

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ mongoose.connect(DB , )
 
 
 const port = process.env.port || 3000 ; 
-app.listen( port , '127.0.0.1' ,  () => {
+const server = app.listen( port , '127.0.0.1' ,  () => {
     console.log(`app runing on port ${port} ...  `)
 })
+
+
+process.on('uhnhandledRejection',err =>{
+console.log(err.name , err.message) ; 
+console.log('UNHANDLED REJECTION !!! SHUTTING DOWN ... ')
+server.close(()=>{
+    process.exit(1) ;
+}) ; 
+ 
+});

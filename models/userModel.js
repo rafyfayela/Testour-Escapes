64918bef8674937881b45c9c74ab1bae56c22174
Rafy Fayela <fayelarafy@gmail.com>
2024-06-27 04:32:48 +0200

    added password reset (generating new reset token and hashing it)

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto') ; 
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs') ; 
@@ -42,7 +43,13 @@ const userSchema = new mongoose.Schema({
     passwordChangedAt : {
         type : Date 
     } , 
-    
+    passwordResetToken : {
+        type : String ,
+        } ,
+    passwordResetExpires : {
+        type : Date ,
+    } ,
+        
 }) ;
 
 
@@ -77,7 +84,13 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     return false;
   };
 
-
+  userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex') ; 
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000 ; //10min 
+    // console.log({resetToken}, this.passwordResetToken) ; 
+    return resetToken ; 
+  } 
 
 const User = mongoose.model('User',userSchema) ; 
 module.exports = User ; 

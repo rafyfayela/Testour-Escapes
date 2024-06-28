df46fcfb210169b6821434a764b245ae8951efc4
Rafy Fayela <fayelarafy@gmail.com>
2024-06-28 15:15:36 +0200

    authentificated user updates his own infos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,40 @@
 const User = require('../models/userModel') ;
 const catchAsync = require('../utils/catchAsync') ; 
+const AppError = require('../utils/appError') ;
+
+
+const filterObj = (obj , ...allowedFields) => {
+    const newObj = {} ; 
+    Object.keys(obj).forEach(el=> {
+        if (allowedFields.includes(el)) newObj[el] = obj [el] ; 
+    })
+    return newObj  ;
+}
+
+// update autentificated user ! 
+exports.updateMe = catchAsync (async(req,res,next) => {  
+    //  create error if user posts password data : 
+    if (req.body.password || req.body.passwordConfirm ) {
+        return next(new AppError('this routes is not for password update ! please use /updateMyPassword' , 400)) ; 
+    }
+    // update user document : 
+                    // filter unwanted data in the req : 
+    const filteredBody = filterObj (req.body , 'name' , 'email' )  ; 
+    
+    const updatedUser =  await User.findByIdAndUpdate(req.user.id , filteredBody , {
+        new : true , 
+        runValidators : true 
+    })
+
+
+    res.status(200).json({
+        status :"succes" , 
+        data : {
+            user : updatedUser 
+        }
+    }); 
+}) ; 
+
 
 
 exports.getallusers = catchAsync( async (req,res) => {

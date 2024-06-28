5406063053fea12a1d806e5c44c0cd033daada20
Rafy Fayela <fayelarafy@gmail.com>
2024-06-28 18:17:39 +0200

    implementing ip rate limiting as security measures !

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express') ;
 const app = express()  ; 
 const morgan = require('morgan') ; 
+const rateLimit = require('express-rate-limit') ; 
 const AppError = require('./utils/appError') ; 
 const globalErrorHandler = require('./controllers/errorController') ; 
 
@@ -11,6 +12,15 @@ console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'developement') {
 app.use(morgan('dev')) ; 
 }
+
+const limiter = rateLimit({
+    max : 150 , 
+    windowMs : 60*60*1000 , 
+    message : 'Too many requests from this IP , please try again later ! '
+})
+
+app.use('/api',limiter); 
+
 app.use(express.json()) ; 
 app.use(express.static('./public/'));
 app.use((req,res,next)=>{
